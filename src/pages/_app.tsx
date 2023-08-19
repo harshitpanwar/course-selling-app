2372dfc6d8a2d237425d9fd1fd411d28f45ae6be
Harshit Panwar <harshitpanwar234567@gmail.com>
2023-08-20 03:46:03 +0530

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
-import './config/recoil'
-import { useEffect } from 'react'
-import { useRecoilState } from 'recoil';
-import { user } from '../../atoms/user';
-
 export default function App({ Component, pageProps }: AppProps) {
 
   return(
